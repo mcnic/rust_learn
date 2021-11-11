931906d9ee4caeaf9fdeca3f1158c6c82ae78408
mcnic <mcnic@ya.ru>
2021-11-11 19:23:12 +0100

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String)
 }
 
-#[derive(Debug)]
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String
+impl IpAddr {
+    fn Debug1(&self) -> String {
+        // &format!("{}.{}.{}.{}", self.0, self.1, self.2, self.3)
+        format!("{}",String::from("123"))
+    }
 }
-
 fn route(ip: &IpAddr) -> String {
     // &ip.address//.push_str(ip.address)
-    format!("'{:#?}' {}", &ip.kind, &ip.address)
+    format!("'{:#?}'", &ip)
 }
 
 fn main() {
-    let ip_v4 = IpAddr{
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1")
-    };
-    let ip_v6 = IpAddr{
-        kind: IpAddrKind::V6,
-        address: String::from("::1")
-    };
-    println!("{:#?}", ip_v4);
-    println!("{:#?}", route(&ip_v6));
+    let ip_v4 = IpAddr::V4(127, 0, 0, 1);
+    let ip_v6 = IpAddr::V6(String::from("::1"));
+
+    // println!("{}", ip_v6);
+    println!("{}", route(&ip_v4));
 }
 
