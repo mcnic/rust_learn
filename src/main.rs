6f729d4dd75d2629827e6a0fe87a5532ba70c670
mcnic <mcnic@ya.ru>
2021-11-30 21:16:46 +0100

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
-use std::fmt;
+use std::str::FromStr;
 
-enum Message {
-    Quit,
-    Input (Option<i32>),
-    // Move {x: i32, y: i32},
-    Write (String),
-    ChangeColor (i32, i32, i32)
-}
-
-impl fmt::Display for Message {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Message::Quit => write!(f, "_"),
-            Message::Write(string) => write!(f, "'{}'", string),
-            Message::ChangeColor(i0, i1, i2) => write!(f, "{}.{}.{}", i0, i1, i2),
-            Message::Input(inp) => match inp {
-                Some(inp_i32) => write!(f, "i32:{}", inp_i32),
-                None => write!(f, "is None")
-            }
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if n > m {
+            let t = m;
+             m = n;
+            n = t;
         }
+        m = m % n;
     }
+    n
 }
 
-fn is_important(mes: &Message) -> String {
-    if let Message::Quit = mes {
-        return String::from("yes")
-    }
-
-    String::from("no")
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                3 * 7 * 11 * 13 * 19),
+            3 * 11);
 }
 
 fn main() {
-    println!("mes0 {}", Message::Quit);
-    println!("mes1 {}", Message::Write(String::from("555")));
-    println!("mes2 {}", Message::ChangeColor(1, 2, 3));
-    println!("mes3 {}", Message::Input(Some("5".parse::<i32>().unwrap())));
+    let mut numbers = Vec::new();
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error convert digit"));
+    }
+    println!("args {:?}", numbers);
 
-    println!("imp {}", is_important(&Message::Quit));
+    println!("{}", gcd(200, 31));
 }
 
