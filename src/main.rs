102709fe1cfe62327aae4e349f9acd0a58f1be8f
mcnic <mcnic@ya.ru>
2022-01-23 21:18:50 +0100

    "right answer" ver 1.2

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,23 @@ use std::cmp::Ordering;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("secret is {}\n", secret_number);
+    println!("input number\n");
+    let mut guess_num: i32;
 
     loop {
-        println!("input number");
-
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("error!");
-        let guess_num: i32 = match guess.trim().parse() {
+        // println!("you tape {}", guess);
+        guess_num = match guess.trim().parse::<i32>() {
             Ok(num) => num,
             Err(_) => continue
         };
-        println!("you tape {}", guess_num);
 
         match guess_num.cmp(&secret_number) {
-            Ordering::Less => println!("too small"),
-            Ordering::Greater => println!("too great"),
+            Ordering::Less => println!("too small ({})", secret_number),
+            Ordering::Greater => println!("too great ({})", secret_number),
             Ordering::Equal => {
                 println!("catch it!");
                 return;
