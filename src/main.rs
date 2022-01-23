1c4742ff5ee6e827ded2a88064c572066c54075a
mcnic <mcnic@ya.ru>
2022-01-23 16:17:54 +0100

    change to learn on online book

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use std::str::FromStr;
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if n > m {
-            let t = m;
-             m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                3 * 7 * 11 * 13 * 19),
-            3 * 11);
-}
+use rand::prelude::*;
 
 fn main() {
-    let mut numbers = Vec::new();
-    for arg in std::env::args().skip(1) {
-        numbers.push(u64::from_str(&arg).expect("error convert digit"));
-    }
-    println!("args {:?}", numbers);
-
-    println!("{}", gcd(200, 31));
+    let mut rng = rand::thread_rng();
+    let y: f64 = rng.gen();
+    println!("rand {}", y);
 }
-
