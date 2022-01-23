6db1cdd5f62036d7c89159a321292ce4ea9f15cf
mcnic <mcnic@ya.ru>
2022-01-23 17:32:57 +0100

    "right answer" ver 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use rand::prelude::*;
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let y: f64 = rng.gen();
-    println!("rand {}", y);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("sectet is {}", secret_number);
+
+    println!("input number\n");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("error!");
+    let mut guess: u32 =  guess
+        .trim()
+        .parse()
+        .expect("not a  number");
+    println!("you tape {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too great"),
+        Ordering::Equal => println!("catch it!"),
+    }
 }
