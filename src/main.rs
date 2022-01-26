6c99bc92d29fb34f9f7d27c355c17830b8ed7aee
mcnic <mcnic@ya.ru>
2022-01-26 20:22:52 +0100

    "struct" ver 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
-use rand::Rng;
-use std::io;
-use std::cmp::Ordering;
+struct Rectangle {
+    height: u32,
+    width: u32
+}
+
+fn sq(r: &Rectangle) -> u32 {
+    r.height * r.width
+}
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("input number\n");
-    let mut guess_num: i32;
+    let r = Rectangle{
+        height:10,
+        width: 20
+    };
 
-    loop {
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("error!");
-        guess_num = match guess.trim().parse::<i32>() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
-
-        match guess_num.cmp(&secret_number) {
-            Ordering::Less => println!("too small ({})", secret_number),
-            Ordering::Greater => println!("too great ({})", secret_number),
-            Ordering::Equal => {
-                println!("catch it!");
-                return;
-            }
-        }
-    }
+    println!("square={}", sq(&r));
 }
---
+++ src/range_match.rs
@@ -0,0 +1,29 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("input number\n");
+    let mut guess_num: i32;
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error!");
+        guess_num = match guess.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match guess_num.cmp(&secret_number) {
+            Ordering::Less => println!("too small ({})", secret_number),
+            Ordering::Greater => println!("too great ({})", secret_number),
+            Ordering::Equal => {
+                println!("catch it!");
+                return;
+            }
+        }
+    }
+}
