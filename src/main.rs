ccdc59c2efb723cdcdcbd0053121903a1f975b94
mcnic <mcnic@ya.ru>
2022-01-31 21:38:07 +0100

    "enum" ver 1.3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String)
 }
 
 fn main() {
-    let ip_1 = IpAddr::V4(String::from("2.2.2.2"));
+    let ip_1 = IpAddr::V4(2 ,2, 2, 2);
     let ip_2 = IpAddr::V6(String::from(":::1"));
     println!("ip1 {:?}\nip_2 {:?}", ip_1, ip_2);
+
+    let o_1 = Option::Some(String::from("ddd"));
+    // let o_2: Option<String> = None;
+    let o_2 = Some(String::from("gghfgh"));
+    println!("opt {:?}, {:?}", o_1, o_2);
+    let res = match o_2 {
+        Some(s) => s,
+        None => String::from("no_ne")
+    };
+    println!("o2 {}", res);
 }
