40a9601690ac356bccc460f49daa137aa120ddcc
mcnic <mcnic@ya.ru>
2022-01-31 20:22:23 +0100

    "enum" ver 1.1

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ enum IpAddrKind {
 
 #[derive(Debug)]
 struct IpAddr {
-    addr: String,
-    ver: IpAddrKind
+    kind: IpAddrKind,
+    address: String,
 }
 
 fn main() {
-    let ip_1 = IpAddr {addr: String::from("2.2.2.2"), ver: IpAddrKind::V4};
-    println!("ip1 {:?}", ip_1);
+    let ip_1 = IpAddr {address: String::from("2.2.2.2"), kind: IpAddrKind::V4};
+    let ip_2 = IpAddr {address: String::from(":::1"), kind: IpAddrKind::V6};
+    println!("ip1 {:?}\nip_2 {:?}", ip_1, ip_2);
 }
