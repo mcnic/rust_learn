3abbb1d71b1f04ed80a7ed064582dfcb12157644
mcnic <mcnic@ya.ru>
2022-02-20 15:28:43 +0100

    "string" ver 1.2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let mut s1 = String::from("строка");
-    s1.push('Я');
-    let s2 = &s1[..];
-    let s3 =  "еще строка".to_string() + &s1;
-    let s4 = format!("{}+{}", "еще строка".to_string(), s1);
-    println!("{}-{}-{}-{}", s1, s2, s3, s4);
+    let hello = "Здравствуйте";
+    let s = &hello[0..2];
+    println!("{}", s);
 }
