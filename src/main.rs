444b6397298074d858bdd4ee5ef9baa0d26bcf34
Max Shatov <shatov@alarstudios.com>
2022-03-24 14:41:16 +0100

    traits 0.2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-fn largest(list: &[i32]) -> i32 {
-    if list.len() == 0 {
-        return 0;
-    }
+// trait MyTrait {
+// }
 
-    let mut larg = list[0];
+fn largest<T>(list: &[T]) -> &T
+    where T: PartialOrd {
+    let mut larg = &list[0];
     for i in list {
         if i > &larg {
-            larg = *i;
+            larg = i;
         }
     }
 
@@ -18,4 +18,8 @@ fn main() {
     let list_1 = [5, 10, 2, 100, 1];
     let max_1 = largest(&list_1);
     println!("max1={:?}", max_1);
+
+    let list_2 = ['a', 'b', 'c'];
+    let max_2 = largest(&list_2);
+    println!("max2={:?}", max_2);
 }
